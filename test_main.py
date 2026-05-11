1d53b5263cf9be14e9799e38b7893a7335364dc5
Sashaio21 <69712540+Sashaio21@users.noreply.github.com>
2026-05-11 22:40:16 +0300

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -3,9 +3,10 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import time
+import sys
 
-
+USERNAME = sys.argv[1] if len(sys.argv) > 1 else "standard_user"
+PASSWORD = sys.argv[2] if len(sys.argv) > 2 else "secret_sauce"
 
 class TestSauceDemo(unittest.TestCase):
 
@@ -27,8 +28,8 @@ class TestSauceDemo(unittest.TestCase):
     def login_and_verify(self):
         self.driver.get("https://www.saucedemo.com/")
 
-        self.driver.find_element(By.ID, "user-name").send_keys("standard_user")
-        self.driver.find_element(By.ID, "password").send_keys("secret_sauce")
+        self.driver.find_element(By.ID, "user-name").send_keys(USERNAME)
+        self.driver.find_element(By.ID, "password").send_keys(PASSWORD)
         self.driver.find_element(By.ID, "login-button").click()
 
         self.wait.until(EC.url_contains("inventory.html"))
