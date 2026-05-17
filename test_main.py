e10b6d76815b2baa1928e4f8d6b67338c0c7164a
Alexandr <alexandrkowal21@gmail.com>
2026-05-17 23:50:40 +0300

    new commit

--- test_main.py
+++ test_main.py
@@ -1,2 +1,22 @@
-def test_sum(): 
-    assert 1 + 1 == 3
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
+import time
+
+driver = webdriver.Chrome()
+driver.get("https://the-internet.herokuapp.com/hovers")
+
+# находим первый блок пользователя
+user = driver.find_element(By.CLASS_NAME, "figure")
+
+# hover
+ActionChains(driver).move_to_element(user).perform()
+
+# маленькая пауза (чтобы UI успел отрисоваться)
+time.sleep(1)
+
+# получаем текст
+caption = driver.find_element(By.CLASS_NAME, "figcaption")
+print(caption.text)
+
+driver.quit()
